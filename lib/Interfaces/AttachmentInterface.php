e5a8a65d055005f6982098f94a6f0e6e77c28e28
Constantin Plotkin <kudrikkk@yandex.ru>
2017-09-20 16:23:48 +0200

    Add attachments interface

---
+++ lib/Interfaces/AttachmentInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MovingImage\Meta\Interfaces;
+
+/**
+ * Interface AttachmentInterface.
+ *
+ */
+interface AttachmentInterface
+{
+    /**
+     * @return string
+     */
+    public function getId();
+
+    /**
+     * @return string
+     */
+    public function getFileName();
+
+    /**
+     * @return string
+     */
+    public function getDownloadUrl();
+
+    /**
+     * @return int
+     */
+    public function getFileSize();
+}
