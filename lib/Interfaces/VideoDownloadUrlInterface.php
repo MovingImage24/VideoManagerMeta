0e10ca2ad468916699ca5ccef06ad425a28d766d
MoTo <mohammadt@moto-office.movingimage24.net>
2019-02-20 16:16:55 +0100

    video download url interface added

---
+++ lib/Interfaces/VideoDownloadUrlInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MovingImage\Meta\Interfaces;
+
+/**
+ * Interface VideoDownloadUrl.
+ */
+class VideoDownloadUrl
+{
+    /**
+     * @return string
+     */
+    public function getQuality()
+    {
+        return $this->quality;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProfileKey()
+    {
+        return $this->profileKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileExtension()
+    {
+        return $this->fileExtension;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFileSize()
+    {
+        return $this->fileSize;
+    }
+}
